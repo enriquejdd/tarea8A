dd1c05902beed34301c789853647a5e1e4f94c15
enriquejdd <enriquejdd1999@gmail.com>
2021-05-05 14:20:53 +0200

    Ejercicio completo

---
+++ src/main/java/tarea8a/Prueba.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea8a;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author enrique
+ */
+public class Prueba {
+    public static void main(String[] args) {
+        
+        Connection con = Conexion.getInstance();
+        
+    }
+}
